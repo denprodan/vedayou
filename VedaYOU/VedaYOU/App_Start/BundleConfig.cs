840dbd3b804bffee9b505f37822914f2a42b131f
DenProdan <darkden88@gmail.com>
2015-06-21 15:25:01 +0300

    add section and article dividers to home page

--- VedaYOU/VedaYOU/App_Start/BundleConfig.cs
+++ VedaYOU/VedaYOU/App_Start/BundleConfig.cs
@@ -8,7 +8,8 @@ namespace VedaYOU.App_Start
         public static void RegisterBundles(BundleCollection collection)
         {
             collection.Add(new ScriptBundle("~/bundles/css").Include("~/Content/Styles.css"));
-            
+            collection.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-1.9.1.js"));
+            collection.Add(new ScriptBundle("~/bundles/IEscripts").Include("~/Scripts/IE/ie.styles.tricks.js"));            
         }
     }
 }
