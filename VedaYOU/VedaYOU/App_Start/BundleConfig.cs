0fc4d344390132056dbefd18066fab6418678c78
DenProdan <darkden88@gmail.com>
2015-09-05 15:21:16 +0300

    add media query for display width 640px

--- VedaYOU/VedaYOU/App_Start/BundleConfig.cs
+++ VedaYOU/VedaYOU/App_Start/BundleConfig.cs
@@ -6,8 +6,7 @@ namespace VedaYOU.App_Start
     public class BundleConfig
     {
         public static void RegisterBundles(BundleCollection collection)
-        {
-           // collection.Add(new ScriptBundle("~/bundles/css").Include("~/Content/Styles.css"));
+        {           
             collection.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-1.9.1.js"));
             collection.Add(new ScriptBundle("~/bundles/fitText").Include("~/Scripts/fitText.js")); 
             collection.Add(new ScriptBundle("~/bundles/IEscripts").Include("~/Scripts/IE/ie.styles.tricks.js"));
